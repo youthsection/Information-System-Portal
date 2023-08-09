e75f647358a7b93fa05e20e557e267b04fd5acaf
youthsection <youthsection@gmail.com>
2023-08-09 09:40:50 +0800

    4th commit
    updating the vision and adding mission and guiding principles

--- src/components/Home.js
+++ src/components/Home.js
@@ -79,60 +79,150 @@ function Home() {
               </motion.h5>
             </blockquote>
           </figure>
+          <div className="row">
+            <div className="col text-center">
+              <motion.p
+                initial={{ x: -200, opacity: 0 }}
+                transition={{ duration: 1.2 }}
+                whileInView={{ opacity: 1, x: 0 }}
+                viewport={{ once: true }}
+                class="lead text-center bg-light text-dark home__p fw-bolder home__vision"
+              >
+                Vision
+              </motion.p>
+            </div>
+          </div>
 
+          <div className="row">
+            <div className="col-md-12 col-sm-12">
+              <motion.p
+                initial={{ x: -200, opacity: 0 }}
+                transition={{ duration: 1.2 }}
+                whileInView={{ opacity: 1, x: 0 }}
+                viewport={{ once: true }}
+                className="text-center "
+              >
+                {" "}
+                Enable, involved, and patriotic youth realizing their
+                aspirations anchored on integrity and compassion{" "}
+              </motion.p>
+              <motion.ul
+                initial={{ x: 200, opacity: 0 }}
+                transition={{ duration: 1.2 }}
+                whileInView={{ opacity: 1, x: 0 }}
+                viewport={{ once: true }}
+                class="list-group "
+              >
+                <li
+                  class="list-group-item text-center active"
+                  aria-current="true"
+                >
+                  Specifically, by 2023, Batan-ong Nabunturanon can be described
+                  as:
+                </li>
+                <li class="list-group-item">
+                  Batan-ong Nabunturan are empowered, vigorous, educated,
+                  patriotic and active both in government and civil society
+                  endeavours living in a peaceful, secure,and socially-inclusive
+                  civilization where respect and equality across gender
+                  preferences, faiths, and cultures exist.
+                </li>
+                <li class="list-group-item">
+                  Batan-ong Nabunturan are engaged in productive economic
+                  activity in the locality having full responsibility on the
+                  access of technology, information, and services for the
+                  betterment of society.
+                </li>
+                <li class="list-group-item">
+                  Bantan-ong Nabunturan have access to technology, information,
+                  and services for the betterment of society.
+                </li>
+              </motion.ul>
+            </div>
+          </div>
+          <br />
           <motion.p
             initial={{ x: -200, opacity: 0 }}
             transition={{ duration: 1.2 }}
             whileInView={{ opacity: 1, x: 0 }}
             viewport={{ once: true }}
-            class="lead text-center bg-light text-dark home__p fw-bolder"
-          >
-            Vision
-          </motion.p>
-
-          <motion.p
-            initial={{ x: 200, opacity: 0 }}
-            transition={{ duration: 1.2 }}
-            whileInView={{ opacity: 1, x: 0 }}
-            viewport={{ once: true }}
-            className="text-center "
-          >
-            By 2026, the Batan-ong Nabunturanon are empowered, fully equipped,
-            globally competent, responsive to climate change, God fearing, and
-            patriotic youth with equal access to quality education and equitable
-            opportunities regardless of social status, diverse sexual
-            orientation, gender identity and expression, religion and customs.
-          </motion.p>
-          <motion.p
-            initial={{ x: -200, opacity: 0 }}
-            transition={{ duration: 1.2 }}
-            whileInView={{ opacity: 1, x: 0 }}
-            viewport={{ once: true }}
-            className="lead text-center bg-light text-dark home__p fw-bolder"
+            className="lead text-center bg-light text-dark home__p fw-bolder home__vision"
           >
             Mission
           </motion.p>
+          <div className="row">
+            <div className="col-md-12 col-sm-12 text-center">
+              <motion.p
+                initial={{ x: 200, opacity: 0 }}
+                transition={{ duration: 1.2 }}
+                whileInView={{ opacity: 1, x: 0 }}
+                viewport={{ once: true }}
+                className="text-center "
+              >
+                Provide equitable access <br />
+                to culturally sensitive, inclusive and meaningful programs that
+                will transform and hone youth as future leaders in the
+                municipality.
+              </motion.p>
+            </div>
+          </div>
+
           <motion.p
-            initial={{ x: 200, opacity: 0 }}
+            initial={{ x: -200, opacity: 0 }}
             transition={{ duration: 1.2 }}
             whileInView={{ opacity: 1, x: 0 }}
             viewport={{ once: true }}
-            className="text-center "
+            className="lead text-center bg-light text-dark home__p fw-bolder home__vision"
           >
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi ipsam
-            esse earum atque et! Sint porro consequuntur doloribus iure. Aut
-            eveniet illo soluta ut nobis eum nam minima optio nemo, quos quis
-            sint rem est aperiam consequatur facere reprehenderit molestias
-            consectetur et quia. Blanditiis ullam natus fugit animi corporis
-            perferendis.
+            Guiding Principles
           </motion.p>
-          {/* <p className="lead text-center bg-light text-dark home__p fw-bolder">
-            Goals
-          </p>
-          <p className="text-center ">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque,
-            commodi?
-          </p> */}
+          <div className="row">
+            <div className="col-md-12 col-sm-12">
+              <motion.p
+                initial={{ x: 200, opacity: 0 }}
+                transition={{ duration: 1.2 }}
+                whileInView={{ opacity: 1, x: 0 }}
+                viewport={{ once: true }}
+                className="text-center "
+              >
+                <span className="fw-bolder">
+                  Patriotic: <br />{" "}
+                </span>{" "}
+                We will strive hardin upholding the best interest and
+                aspirations of Davao de Oro Youth in realizing endeavours for
+                personal and professional growth and the betterment of the
+                nation.
+              </motion.p>
+              <motion.p
+                initial={{ x: -200, opacity: 0 }}
+                transition={{ duration: 1.2 }}
+                whileInView={{ opacity: 1, x: 0 }}
+                viewport={{ once: true }}
+                className="text-center "
+              >
+                <span className="fw-bolder">
+                  Involved: <br />
+                </span>{" "}
+                We will ensure proactive participation of the youth in the
+                planning, implementation and evaluation of youth programs. Thus,
+                engaging the youth to take part voluntarily in any government
+                initiatives.
+              </motion.p>
+              <motion.p
+                initial={{ x: 200, opacity: 0 }}
+                transition={{ duration: 1.2 }}
+                whileInView={{ opacity: 1, x: 0 }}
+                viewport={{ once: true }}
+                className="text-center "
+              >
+                <span className="fw-bolder">
+                  Enabled: <br />
+                </span>{" "}
+                We will developed the youth to become empowered that will enable
+                them to take actions on societal issues concerning them.
+              </motion.p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
