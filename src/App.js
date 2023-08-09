a96017c874a6aab2584e5c79e2521f97af3dca4c
youthsection <youthsection@gmail.com>
2023-08-09 09:45:29 +0800

    5th commit
    installing framer motion and react router dom

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MunicipalYouthDevelopmentCouncil from "./components/MunicipalYouthDevelopmentCouncil";
 import SangguniangKabataan from "./components/SangguniangKabataan";
+import Navbar from "./components/Navbar";
+import YouthSection from "./components/YouthSection";
+import VisionMission from "./components/VisionMission";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <div className="App">
-      <figure className="text-center">
-        <blockquote className="blockquote">
-          <p>YOUTH INFORMATION SYSTEM</p>
-        </blockquote>
-        <figcaption className="blockquote-footer">
-          Municipality of Nabunturan
-        </figcaption>
-      </figure>
-      <hr className="border border-success border-2" />
-      <MunicipalYouthDevelopmentCouncil />
-      <hr className="border border-success border-2" />
-      <SangguniangKabataan />
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+
+        {/* <hr className="border border-success border-2" />
+        <MunicipalYouthDevelopmentCouncil /> */}
+        {/* <hr className="border border-success border-2" />
+        <SangguniangKabataan /> */}
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route
+            exact
+            path="/mypdc"
+            element={<MunicipalYouthDevelopmentCouncil />}
+          />
+          <Route
+            exact
+            path="/sangguniang-kabataan"
+            element={<SangguniangKabataan />}
+          />
+          <Route exact path="/youth-section" element={<YouthSection />} />
+          <Route exact path="/vision-mission" element={<VisionMission />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
